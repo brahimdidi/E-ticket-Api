34fc79bcfe2d7c7d03e1799c0b6507c7c1d56f60
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-06 18:58:06 +0100

    Add the event_id to the reservation table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_06_175434) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_06_175634) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_06_175434) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
+    t.index ["event_id"], name: "index_reservations_on_event_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
@@ -39,5 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_06_175434) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reservations", "events"
   add_foreign_key "reservations", "users"
 end
