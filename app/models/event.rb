7df1137968fcf319f28ca310eeb34443680a02ba
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-06 19:11:54 +0100

    Fix linter errors

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   has_many :reservations
   validates :name, :photo, :location, :event_date, presence: true
-  validates :ticket_price,:seats_available, numericality: { greater_than_or_equal_to: 0 }
+  validates :ticket_price, :seats_available, numericality: { greater_than_or_equal_to: 0 }
 end
