693918e557bad9adeea23690b91338e5c36512e8
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-06 19:10:02 +0100

    Add validation to the ticket and seats attributs

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,4 @@
 class Reservation < ApplicationRecord
-    belongs_to :user,:event
-    validates :reservation_date, presence: true
+  belongs_to :user, :event
+  validates :reservation_date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-    has_many :reservations
-    validates :username, presence: true, length: { in: 1..100 }
+  has_many :reservations
+  validates :username, presence: true, length: { in: 1..100 }
 end
