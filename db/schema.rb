97fe37127f41a7aad1b69d29cca9778ed8f42647
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-06 18:53:40 +0100

    Create the reservation table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_06_174743) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_06_175227) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_06_174743) do
     t.string "location"
     t.integer "ticket_price"
     t.integer "seats_available"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.datetime "reservation_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
