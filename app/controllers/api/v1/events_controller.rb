16f2a71460804526e7e21e9ed5ebea5883b4f4db
Ezeoke Stanley <ezeokestanley.ifeanyi@gmail.com>
2022-10-07 16:45:35 +0100

    Fix rubocop linters error

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,27 +1,27 @@
 class Api::V1::EventsController < ApplicationController
   def index
     events = Event.all
-    render "api/events/index"
+    render 'api/events/index'
   end
 
   def show
     event = Event.find(params[:id])
-    render "api/events/show"
+    render 'api/events/show'
   end
 
   def create
     @event = Event.new(event_params)
     if @event.after_save
-        render "api/events/show"
+      render 'api/events/show'
     else
       render json: { error: 401, message: ' event cannot be processed !' }
-      end
     end
+  end
 
   def update
     @event = Event.find(params[:id])
     if @event.update_attributes(event_params)
-      render "api/events/show"
+      render 'api/events/show'
     else
       render json: { error: 401, message: ' Operation did not succeed!' }
     end
@@ -30,7 +30,7 @@ class Api::V1::EventsController < ApplicationController
   def destroy
     @event = Event.find(params[:id])
     if @event.destroy
-        render "api/events/show"
+      render 'api/events/show'
     else
       render json: { error: 401, message: ' Operation did not succeed!' }
     end
@@ -39,14 +39,14 @@ class Api::V1::EventsController < ApplicationController
   private
 
   def event_params
-      params.require(:event).permit(
-        :name,
-        :description,
-        :event_date,
-        :photo,
-        :location,
-        :ticket_price,
-        :seats_available
-      )
+    params.require(:event).permit(
+      :name,
+      :description,
+      :event_date,
+      :photo,
+      :location,
+      :ticket_price,
+      :seats_available
+    )
   end
 end
