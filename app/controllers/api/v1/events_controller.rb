7522fdf33906fc2f736c9355fe5856839f974348
Ezeoke Stanley <ezeokestanley.ifeanyi@gmail.com>
2022-10-07 14:13:38 +0100

    Add methods for create and update events in controller

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,13 +1,29 @@
 class Api::V1::EventsController < ApplicationController
   def index
     events = Event.all
-    render json: events
+    render "api/events/index"
   end
 
   def show
     event = Event.find(params[:id])
-    render json: event
+    render "api/events/show"
   end
 
-  
+  def create
+    @event = Event.new(event_params)
+    if @event.after_save
+        render "api/events/show"
+    else
+        render json: @event.error.full_messages, status: 422
+    end
+
+    def update
+      @event = Event.find(params[:id])
+      if @event.update_attributes(event_params)
+        render "api/events/show"
+      else
+        render.json: @event.error.full_messages, status: 422
+      end
+
+
 end
