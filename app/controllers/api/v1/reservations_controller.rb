7587747215c50b33dd951fb60947ae6acfc8850f
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-10 19:15:30 +0100

    Add the destroy reservation action

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,11 +1,15 @@
 class Api::V1::ReservationsController < ApplicationController
   def index
-    @reservations = Reservation.all.order("created_at DESC")
-    render json: @reservations 
+    @reservations = User.find(params[:user_id]).reservations.order("created_at DESC")
+    @reservations_list = []
+    @reservations.each do |res|
+      @reservations_list << { reservation: res, event: Event.find(res.event_id) }
+    end
+    render json: { reservation: @reservations_list }
   end
 
   def create
-    @reservations = @reservations.create(reservation_param)
+    @reservation = Reservation.create(reservation_param)
     if @reservation.save
       render json: { message: 'Reservation was created' }
     else
@@ -14,10 +18,17 @@ class Api::V1::ReservationsController < ApplicationController
   end
 
   def destroy
+    @reservation = Reservation.find(params[:id])
+    Reservation.destroy_by(id: params[:id])
+    if @reservation.destroy
+      render  json: { message: "Reservation Destroyed succefuly !" }
+    else
+      render json: { error: 401, message: ' Operation did not succeed!' }
+    end
   end
 
   private
   def reservation_param
-    params.require(:reservations).permit(:reservation_date, :user_id,:event_id)
+    params.require(:reservation).permit(:reservation_date, :user_id,:event_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [:index, :create, :show, :destroy]
-      resources :reservations, only: [:index, :create, :destroy]
+      resources :users, only: [:index, :create, :show, :destroy] do
+        resources :reservations, only: [:index, :create, :destroy]
+      end
       resources :events, only: [:index, :show, :create, :update, :destroy]
     end
   end
