c8771db99ba62f5f4a1d9fa3551303b15b7db090
brahimdidi <brahimgb1team@gmail.com>
2022-10-11 13:32:22 +0000

    migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_10_171513) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_11_132939) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_171513) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "reservations", force: :cascade do |t|
@@ -45,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_171513) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "events", "users"
   add_foreign_key "reservations", "events"
   add_foreign_key "reservations", "users"
 end
