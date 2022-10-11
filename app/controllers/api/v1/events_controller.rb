f6ae95ef5893dfeaf2130f30c37e395a981803bb
brahimdidi <brahimgb1team@gmail.com>
2022-10-11 14:03:13 +0000

    typo in events

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -22,7 +22,7 @@ class Api::V1::EventsController < ApplicationController
     @event = Event.find(params[:id])
     Event.destroy_by(id: params[:id])
     if @event.destroy
-      render json: { message: "#{@event.name} Destroyed succefuly !" }
+      render json: { message: "Event #{@event.name} Destroyed succefuly !" }
     else
       render json: { error: 401, message: ' Operation did not succeed!' }
     end
