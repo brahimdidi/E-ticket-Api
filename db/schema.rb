9111eb02ab87ea6b490700ccf43b31c96d383b9b
brahimdidi <brahimgb1team@gmail.com>
2022-10-11 14:00:55 +0000

    migrate the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_132939) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_11_135819) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_132939) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.bigint "user_id", null: false
+    t.index ["name"], name: "index_events_on_name", unique: true
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
