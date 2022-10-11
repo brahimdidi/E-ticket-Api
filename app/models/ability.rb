c86b9512d65a47de5410eced8fedd6464d9c1c81
brahimdidi <brahimgb1team@gmail.com>
2022-10-11 11:10:57 +0000

    fix linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
@@ -9,7 +7,7 @@ class Ability
       can :manage, :all
     else
       can :read, :all
-      can :manage, Reservation, user: user
+      can :manage, Reservation, user:
     end
 
     # Define abilities for the user here. For example:
