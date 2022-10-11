8de58d6ea0ea9b8ab19ab825f5939aabd31d6390
brahimdidi <brahimgb1team@gmail.com>
2022-10-11 17:04:20 +0000

    add tests for use model : new, validations, uniqueness

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before :all do
+    @user = User.create(username 'test')
+  end
+
+  it 'is valid and added to users' do
+    expect(User.all).to include @user
+    expect(@user).to be_valid
+  end
+
+  it 'useraname should be correct' do
+    expect(@user.username).to eql 'test'
+  end
+  it 'is not valid without a username' do
+    user = User.create()
+    expect(user).to_not be_valid
+  end
+  it 'username must be unique' do 
+    user = User.create('test')
+    expect(User.all).to_not include user
+    p user.errors
+  end
+end
