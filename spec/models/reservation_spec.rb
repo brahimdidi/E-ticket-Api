a1cfb0a97ee6e89a9a34fd52dda196b1cf6ba0e8
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-11 16:54:39 +0100

    Fix linter errors

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
   before :all do
-    @user = User.create(username: "mohamed")
+    @user = User.create(username: 'mohamed')
     @event = Event.create(name: 'ev2', event_date: '2022-12-12', photo: 'test', location: 'constantine',
-        ticket_price: 200, seats_available: 230, user_id: @user.id)
-    @reservation = Reservation.new( reservation_date: '2022-09-04' ,user_id: @user.id, event_id: @event.id)
+                          ticket_price: 200, seats_available: 230, user_id: @user.id)
+    @reservation = Reservation.new(reservation_date: '2022-09-04', user_id: @user.id, event_id: @event.id)
   end
 
   it 'is valid' do
