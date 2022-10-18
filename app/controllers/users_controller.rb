16d178a736567311250f4aca9bfdab7a275a721c
Ezeoke Stanley <ezeokestanley.ifeanyi@gmail.com>
2022-10-18 21:19:03 +0100

    Add routes to app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,6 +50,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username)
+    params.require(:user).permit(:username, :email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post 'api/v1/register/:username, to: 'users#register'
+  post 'api/v1/register/:username/:email', to: 'users#register'
   get 'api/v1/login/:username', to: 'users#login'
   get 'api/v1/users/event/:id', to: 'users#user_event'
   get 'splash', to: 'users#register'
@@ -18,3 +18,4 @@ Rails.application.routes.draw do
   end
   # Defines the root path route ("/")
   # root "articles#index"
+end
