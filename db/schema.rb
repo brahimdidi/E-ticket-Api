04d3e37060f85b2f83ae05633576b73b1666df07
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-18 22:05:18 +0100

    Add email and password to the user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_135819) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_204713) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_135819) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role"
+    t.string "email"
+    t.string "password_digest"
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
